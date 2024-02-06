2b4b6ad2120d279ad83c992be98c6772313ea3d7
Dibyas98 <dibyashaktip98.com>
2024-02-06 23:10:08 +0530

    modal

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,34 +1,51 @@
-import React, { useContext } from 'react'
-import { myContext } from '../../context/Data'
-import CartProduct from './CartProduct'
-import CartTotal from './CartTotal'
-import { useSelector } from 'react-redux'
-
+import React, { useContext, useState,useEffect } from "react";
+import { myContext } from "../../context/Data";
+import CartProduct from "./CartProduct";
+import CartTotal from "./CartTotal";
+import { useSelector } from "react-redux";
 
 export default function Cart() {
-    const {mode} = useContext(myContext)
-    const cartList = useSelector((store) => store.cart.cartdata);
-    console.log(cartList.length);
+  const { mode } = useContext(myContext);
+  const cartList = useSelector((store) => store.cart.cartdata);
+  const [totalAmount, setTotalAmount] = useState(0);
+  useEffect(() => {
+    if (cartList.length > 0) {
+      let total = 0;
+      cartList.forEach((ele) => {
+        let bal = parseFloat(ele.price.split("$")[1]); // Convert to number
+        total += bal;
+      });
+      setTotalAmount(total);
+    } else {
+      setTotalAmount(0); // Reset total amount if cart is empty
+    }
+  }, [cartList]); // Run this effect whenever cartList changes
+
+  console.log(totalAmount);
   return (
-    <div className="h-auto pt-5 bg-gray-100 " style={{ backgroundColor: mode === 'dark' ? '#282c34' : '', color: mode === 'dark' ? 'white' : '', }}>
-    <h1 className="mb-10 text-2xl font-bold text-center">Cart Items</h1>
-    <div className="justify-center max-w-5xl px-6 mx-auto md:flex md:space-x-6 xl:px-0 ">
-        
+    <div
+      className="h-auto pt-5 bg-gray-100 "
+      style={{
+        backgroundColor: mode === "dark" ? "#282c34" : "",
+        color: mode === "dark" ? "white" : "",
+      }}
+    >
+      <h1 className="mb-10 text-2xl font-bold text-center">Cart Items</h1>
+      <div className="justify-center max-w-5xl px-6 mx-auto md:flex md:space-x-6 xl:px-0 ">
         {/* cartproduct */}
         <div className="flex-col justify-center md:w-2/3">
-  {cartList.length > 0 ? (
-    cartList.map((ele) => {
-      return <CartProduct key={ele.asin} />;
-    })
-  ) : (
-    <h1>No Product Add</h1>
-  )}
-</div>
+          {cartList.length > 0 ? (
+            cartList.map((ele) => {
+              return <CartProduct key={ele.asin} />;
+            })
+          ) : (
+            <h1>No Product Add</h1>
+          )}
+        </div>
 
-      
         {/* carttotal */}
-      <CartTotal/>
+        <CartTotal total={totalAmount}/>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
--- src/pages/cart/CartTotal.jsx
+++ src/pages/cart/CartTotal.jsx
@@ -1,32 +1,92 @@
-import React, { useContext } from 'react'
+import React, { useContext,useState } from 'react'
 import { myContext } from '../../context/Data'
+import Modal from './Model'
 
-export default function CartTotal() {
+export default function CartTotal({total}) {
     const {mode} = useContext(myContext)
+    const [name, setName] = useState("")
+    const [address, setAddress] = useState("");
+    const [pincode, setPincode] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+  
+  
+    const buyNow = async () => {
+      // validation 
+      if (name === "" || address == "" || pincode == "" || phoneNumber == "") {
+        return toast.error("All fields are required", {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        })
+      }
+    }
+
+    const addressInfo = {
+      name,
+      address,
+      pincode,
+      phoneNumber,
+      date: new Date().toLocaleString(
+        "en-US",
+        {
+          month: "short",
+          day: "2-digit",
+          year: "numeric",
+        }
+      )
+    }
+
+    console.log(addressInfo)
+
+  //   var options = {
+  //     key: "",
+  //     key_secret: "",
+  //     amount: parseInt(1 * 100),
+  //     currency: "INR",
+  //     order_receipt: 'order_rcptid_' + name,
+  //     name: "E-Bharat",
+  //     description: "for testing purpose",
+  //     handler: function (response) {
+  //         console.log(response)
+  //         toast.success('Payment Successful')
+  //     },
+  
+  //     theme: {
+  //         color: "#3399cc"
+  //     }
+  // };
+  
+  // var pay = new window.Razorpay(options);
+  // pay.open();
+  // console.log(pay)
+
+
+    
   return (
     <div className="sticky h-full p-6 mt-6 mb-5 bg-white border rounded-lg shadow-md md:mt-0 md:w-1/3 top-28" style={{ backgroundColor: mode === 'dark' ? 'rgb(32 33 34)' : '', color: mode === 'dark' ? 'white' : '', }}>
         <div className="flex justify-between mb-2">
           <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Subtotal</p>
-          <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹100</p>
+          <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>{total}</p>
         </div>
         <div className="flex justify-between">
           <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Shipping</p>
-          <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹20</p>
+          <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>{parseFloat(total*(1/20)).toFixed(2)}</p>
         </div>
         <hr className="my-4" />
         <div className="flex justify-between mb-3">
           <p className="text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>Total</p>
           <div className=''>
-            <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>₹200</p>
+            <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>{parseFloat(total+(total*(1/20))).toFixed(2)}</p>
           </div>
         </div>
-        {/* <Modal  /> */}
-        <button
-          type="button"
-          className="w-full py-2 font-bold text-center text-white rounded-lg bg-violet-600 "
-        >
-          Buy Now
-        </button>
+        <Modal name={name} address={address} pincode={pincode} phoneNumber={phoneNumber} setName={setName} setAddress={setAddress} setPincode={setPincode} setPhoneNumber={setPhoneNumber} buyNow={buyNow} />
+            
+        
       </div>
   )
 }
