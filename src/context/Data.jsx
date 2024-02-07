e380aabc1ce04625a323b9ac0cf1f1cbe479b8a6
Dibyas98 <dibyashaktip98.com>
2024-02-07 20:36:37 +0530

    search optimize

--- src/context/Data.jsx
+++ src/context/Data.jsx
@@ -39,7 +39,8 @@ export default function Data({ children }) {
 
   };
   const handelSearch = (arg) =>{
-    setSearch(arg)
+    // setSearch(arg)
+   
     fetchData(arg).then(data => dispatch(getSearchData(data)))
   }
 
