3dc79988174327ba2492bd9b98f3c6c68c190e1d
Dibyas98 <dibyashaktip98.com>
2024-02-27 18:22:04 +0530

    redirec

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -44,7 +44,7 @@ export default function Login() {
             setloading(false);
             const cart = doc(fireDB,'cart',res.user.uid);
             const cartSnap =(await getDoc(cart)).data();
-            dispatch(getCartLogin(cartSnap))
+            // dispatch(getCartLogin(cartSnap))
             // console.log(cartSnap);
             return navigate('/')
             
